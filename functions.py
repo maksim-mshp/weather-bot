f17e058b3ef43e93587452c8caa491561a8f9e44
maksim-mshp <maksim-mshp@ya.ru>
2021-02-22 22:12:24 +0300

    [+] Добавление одежды
    [+] Просмотр моей одежды

--- functions.py
+++ functions.py
@@ -2,26 +2,314 @@ import requests
 import json
 from config import *
 
-def get_weather_city(city):
-	url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={weather_token}&lang=ru'
-	response = requests.get(url)
+def deemojify(text):
+	import re
+	regrex_pattern = re.compile(pattern = "["
+		u"\U0001F600-\U0001F64F"  # emoticons
+		u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+		u"\U0001F680-\U0001F6FF"  # transport & map symbols
+		u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+								"]+", flags = re.UNICODE)
+	return regrex_pattern.sub(r'',text)
 
-	return json.loads(response.text)
+def average_list(list):
+	return sum(list) / len(list)
+
+def average_weather(api_data):
+	av_temp_list = []
+	av_feels_list = []
+	av_humidity_list = []
+
+	av_temp_list.append(api_data['current']['temp'])
+	av_feels_list.append(api_data['current']['feels_like'])
+	av_humidity_list.append(api_data['current']['humidity'])
+
+	for idx in range(11):
+		av_temp_list.append(api_data['hourly'][idx]['temp'])
+		av_feels_list.append(api_data['hourly'][idx]['feels_like'])
+		av_humidity_list.append(api_data['hourly'][idx]['humidity'])
+
+	result = (
+		int(average_list(av_temp_list)),
+		int(average_list(av_feels_list)),
+		int(average_list(av_humidity_list))
+	)
+
+	return result
+
+def get_weather_description(api_data):
+	umbrella = False
+
+	# Определяем дождь
+	max_rain_wcode = '000'
+	max_rain_description = -1
+	add_code = '000'
+	add_description = -1
+	snow = False
+	api_data['hourly'].insert(0, api_data['current'])
+	for idx in range(12):
+		wcode = str(api_data['hourly'][idx]['weather'][0]['id'])
+		wdesc = api_data['hourly'][idx]['weather'][0]['description']
+
+		if (wcode == '615' or wcode == '616'):
+			max_rain_wcode = wcode
+			max_rain_description = wdesc
+			snow = True
+			umbrella = True
+			break
+		
+		if (wcode[0] == '2' or wcode[0] == '3' or wcode[0] == '5'):
+			umbrella = True
+			max_rain_wcode = wcode
+			max_rain_description = wdesc
+
+		if (wcode[0] == '6'):
+			snow = True
+			add_code = wcode
+			add_description = wdesc
+			break
+
+			if (int(wcode) > int(add_code)):
+				add_code = wcode
+				add_description = wdesc
+			else:
+				if (add_code[0] != '6'):
+					add_code = wcode
+					add_description = wdesc
+		else:
+			if (wcode[0] == '7'):
+				if (int(wcode) > int(add_code)):
+					add_code = wcode
+					add_description = wdesc
+			else:
+				if (add_code[0] != '7'):
+					add_code = wcode
+					add_description = wdesc
+				else:
+					if (add_code[0] == '8'):
+						if (int(wcode) > int(add_code)):
+							add_code = wcode
+							add_description = wdesc
+
+	if (max_rain_description == -1):
+		res_description = add_description
+	else:
+		res_description = max_rain_description
+
+	return (res_description, umbrella, snow)
+
+def get_clothes(temp, feels_like):
+	aver = av_temp = int((temp + feels_like) / 2)
+
+	if (aver <= -20):
+		return (1, True, 0, 0, 0)
+	elif (-20 < aver <= -10):
+		return (2, True, 1, 0, 0)
+	elif (-10 < aver <= 5):
+		return (3, True, 2, 1, 1)
+	elif (5 < aver <= 15):
+		return (4, False, 3, 1, 2)
+	elif (15 < aver <= 20):
+		return (0, False, 4, 1, 3)
+	else:
+		return (5, False, 5, 2, 3)
+
+def get_img_url(temp, feels_like, rain, snow):
+	av_temp = int((temp + feels_like) / 2)
+	link = 'https://maksim.cherny.sh/weather_bot_images/'
+
+	if (snow):
+		link += '28.jpg'
+		sourse = 'https://unsplash.com/photos/5CqcTSl7UH0'
+	elif (rain):
+		link += '27.jpg'
+		sourse = 'https://unsplash.com/photos/tjbXOTqM0U8'
+	else:
+		if (av_temp <= -20):
+			link += '24.jpg'
+			sourse = 'https://unsplash.com/photos/9KAnqm0mTxc'
+		elif (-20 < av_temp <= -10):
+			link += '25.jpg'
+			sourse = 'https://unsplash.com/photos/i69IbnCdxRs'
+		elif (-10 < av_temp <= 5):
+			link += '26.jpg'
+			sourse = 'https://unsplash.com/photos/Lss2BdwBKho'
+		elif (5 < av_temp <= 15):
+			link += '23.jpg'
+			sourse = 'https://unsplash.com/photos/MrETbReEVjw'
+		elif (15 < av_temp <= 20):
+			link += '22.jpg'
+			sourse = 'https://unsplash.com/photos/8wJ5XMpepG8'
+		else:
+			link += '21.jpg'
+			sourse = 'https://unsplash.com/photos/NDCy2-9JhUs'
+
+	return (link, sourse)
+
+def get_msg_text(data, snow):
+	clothes_desc = []
+
+	text = f"<u><b>Погода:</b></u> сегодня будет {data['weather']['description']}, "
+	text += f"{data['weather']['temp']} °C, ощущается как {data['weather']['feels_like']} °C. "
+	text += f"Влажность составит {data['weather']['humidity']}%.\n\n"
+	img_data = get_img_url(data['weather']['temp'], data['weather']['feels_like'], data['clothes']['umbrella'], snow)
+	img_url = img_data[0]
+	text += f"<a href='{img_url}'>&#8205;</a>"
+
+	headdress_d = {
+		1: 'тёплую шапку',
+		2: 'зимнюю шапку',
+		3: 'шапку',
+		4: 'шапку (можно без неё)',
+		5: 'кепку'
+	}
+
+	outerwear_d = {
+		0: 'очень тёплую куртку',
+		1: 'зимнюю куртку',
+		2: 'лёгкую куртку',
+		3: 'ветровку или худи',
+		4: 'худи',
+		5: 'футболку'
+	}
+
+	pants_d = {
+		0: 'тёплые штаны',
+		1: 'джинсы',
+		2: 'шорты',
+	}
+
+	shoes_d = {
+		0: 'сапоги',
+		1: 'тёплые ботинки',
+		2: 'ботинки или кроссовки',
+		3: 'кроссовки',
+	}
+
+	try:
+		clothes_desc.append(headdress_d[data['clothes']['headdress']])
+	except KeyError as e:
+		pass
+
+	if (data['clothes']['sweater']):
+		clothes_desc.append('свитер')
+
+	try:
+		clothes_desc.append(outerwear_d[data['clothes']['outerwear']])
+	except KeyError as e:
+		pass
+
+	try:
+		clothes_desc.append(pants_d[data['clothes']['pants']])
+	except KeyError as e:
+		pass
+
+	try:
+		clothes_desc.append(shoes_d[data['clothes']['shoes']])
+	except KeyError as e:
+		pass
+
+	if (data['clothes']['umbrella']):
+		clothes_desc.append('взять с собой зонтик или дождевик')
+
+	text += "<u><b>Сегодня стоит надеть</b></u> " + ', '.join(clothes_desc[:-2]) + ', ' + clothes_desc[-2] + ' и ' + clothes_desc[-1]
+	text += f"\n\nИсточник картинки: <a href='{img_data[1]}'>Unsplash</a>"
+	return text
+
+def generate_message(lat, lon):
+	url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&lang=ru&exclude=minutely,daily,alerts&appid={weather_token}'
+	response = requests.get(url)
+	api_data = json.loads(response.text)
+
+	average_data = {
+		'weather': {
+			'temp': -1,
+			'feels_like': -1,
+			'humidity': -1,
+			'description': '',
+		}, 
+		'clothes': {
+			# 0 - ничего, 1 - теплая шапка, 2 - шапка зимняя, 3 - шапка, 4 - шапка (можно и без неё), 5 - кепка
+			'headdress': -1, 
+			'sweater': False,
+			# 0 - очень теплая, 1 - зимняя, 2 - ветровка, 3 - ветровка/худи, 4 - худи, 5 - футболка
+			'outerwear': -1,
+			# 0 - теплые, 1 - джинсы, 2 - шорты
+			'pants': -1,
+			# 0 - сапоги, 1 - теплые ботинки, 2 - ботинки/кроссовки, 3 - кроссовки
+			'shoes': -1,
+			'umbrella': False
+		}
+	}
+
+	# Средние значения
+	av = average_weather(api_data)
+	average_data['weather']['temp'] = av[0]
+	average_data['weather']['feels_like'] = av[1]
+	average_data['weather']['humidity'] = av[2]
+
+	weather_desc = get_weather_description(api_data)
+	average_data['weather']['description'] = weather_desc[0]
+	average_data['clothes']['umbrella'] = weather_desc[1]
+
+	clothes = get_clothes(av[0], av[1])
+
+	average_data['clothes']['headdress'] = clothes[0]
+	average_data['clothes']['sweater'] = clothes[1]
+	average_data['clothes']['outerwear'] = clothes[2]
+	average_data['clothes']['pants'] = clothes[3]
+	average_data['clothes']['shoes'] = clothes[4]
+
+
+	return get_msg_text(average_data, weather_desc[2])
+
+def get_clothes_description(c_type, c_thing):
+	types = ('головной убор', 'верхняя одежда', 'верхняя одежда', 'штаны', 'обувь')
+	clothes = (
+		{
+			1: 'тёплая шапка',
+			2: 'зимняя шапка',
+			3: 'шапка',
+			4: 'шапка (можно и без неё)',
+			5: 'кепка'
+		}, {
+			1: 'свитер'
+		}, {
+			0: 'очень тёплая куртка',
+			1: 'зимняя куртка',
+			2: 'ветровка',
+			3: 'ветровка или худи',
+			4: 'худи',
+			5: 'футболка'
+		}, {
+			0: 'тёплые штаны',
+			1: 'джинсы',
+			2: 'шорты',
+		}, {
+			0: 'сапоги',
+			1: 'тёплые ботинки',
+			2: 'ботинки или кроссовки',
+			3: 'кроссовки',
+		}
+	)
 	
-def get_weather_location(lat, lon):
-	url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={weather_token}&lang=ru'
-	response = requests.get(url)
+	d_type = 'неизвестно'
+	d_thing = 'неизвестно'
+	
+	try:
+		d_type = types[c_type]
+	except IndexError:
+		pass
 
-	return json.loads(response.text)
+	try:
+		try:
+			d_thing = clothes[c_type][c_thing]
+		except IndexError:
+			pass
+	except KeyError:
+		pass
 
-def get_forecast_city(city):
-	url = f'http://api.openweathermap.org/forecast/2.5/forecast?q={city}&units=metric&appid={weather_token}&lang=ru'
-	response = requests.get(url)
+	return (d_type, d_thing)
 
-	return json.loads(response.text)
 
-def get_forecast_location(lat, lon):
-	url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={weather_token}&lang=ru'
-	response = requests.get(url)
 
-	return json.loads(response.text)
--- main.py
+++ main.py
@@ -4,24 +4,48 @@ import requests
 import json
 from telebot import types
 from tendo import singleton
+from geopy.geocoders import Nominatim as geolocation
+from datetime import datetime
+import threading as th
+import math
 
 # Импорт настроек (токены + настройки для подключения к БД)
 from config import *
 from functions import *
 
-me = singleton.SingleInstance()
 connection = sql.connect(
     user = db_config['user'],
     password = db_config['password'],
     unix_socket = db_config['unix_socket'],
     database = db_config['database']
 )
-db = connection.cursor()
 
+db = connection.cursor()
 bot = telebot.TeleBot(TOKEN)
 
-def confirm():
-	connection.commit()
+main_kb = types.ReplyKeyboardMarkup(True)
+main_kb.add(
+	types.KeyboardButton('👔 Что надеть?'),
+	types.KeyboardButton('➕ Добавить одежду')
+)
+main_kb.row(
+	types.KeyboardButton('🗄️ Моя одежда'),
+	types.KeyboardButton('🏠 Сменить город')
+)
+main_kb.row(
+	types.KeyboardButton('⏰ Расписание'),
+	types.KeyboardButton('✏️ Редактировать одежду')
+)
+
+no_kb = types.ReplyKeyboardRemove()
+
+##################### НАЧАЛО РАСПИСАНИЯ ########################
+
+def send_success(uuid):
+	bot.send_message(uuid, f"Успешно ✅", reply_markup = main_kb)
+def send_idk(uuid):
+	bot.send_message(uuid, f"Я не знаю что ответить 😢", reply_markup = main_kb)
+
 def get_screen(uuid):
 	st = "SELECT screen FROM wb_users WHERE tg_id = %s"
 	vals = [uuid]
@@ -34,70 +58,107 @@ def check_active(uuid):
 	active = db.fetchall()[0][0]
 
 	if (active == 0):
-		bot.send_message(uuid, "⚠ Ваш профиль неактивен! Отправьте /activate для активации")
+		bot.send_message(uuid, "⚠ Ваш профиль неактивен! Отправьте /activate для активации", reply_markup = no_kb)
 		return False
 	return True
-def send_success(uuid):
-	bot.send_message(uuid, f"Успешно ✅")
-def send_idk(uuid):
-	bot.send_message(uuid, f"Я не знаю что ответить 😢")
+def send_weather(uuid):
+	if check_active(uuid):
+		if get_screen(uuid) == 0:
+			st = "SELECT lat, lon FROM wb_users WHERE tg_id = %s"
+			vals = [uuid]
+			db.execute(st, vals)
+
+			data = db.fetchall()[0]
+			lat = data[0]
+			lon = data[1]
+
+			message = generate_message(lat, lon)
+			
+			bot.send_message(uuid, message, parse_mode='html', reply_markup = main_kb)
+		else:
+			send_idk(uuid)
+
+
+def send_schedule():
+	now = datetime.now()
+	h, m = now.hour, now.minute
+	st = "SELECT user FROM wb_schedule WHERE time = '%s:%s:00'"
+	vals = [h, m]
+	db.execute(st, vals)
+	for item in db.fetchall():
+		send_weather(item[0])
+
+thr = th.Thread(target=send_schedule, args=())
+thr.start()
+
+me = singleton.SingleInstance()
+
+##################### КОНЕЦ РАСПИСАНИЯ ########################
+
+def confirm():
+	connection.commit()
+
+def set_type(id, ctype):
+	st = "UPDATE wb_clothes set type = %s WHERE id = %s"
+	vals = [ctype, id]
+	db.execute(st, vals)
+	confirm()
+
+def set_thing(id, cthing):
+	st = "UPDATE wb_clothes set thing = %s WHERE id = %s"
+	vals = [cthing, id]
+	db.execute(st, vals)
+	confirm()
+
+def get_clothes_info(id):
+	st = "SELECT * FROM wb_clothes WHERE id = %s"
+	vals = [id]
+	db.execute(st, vals)
+	res = db.fetchall()[0]
+	return (res[1], res[2], res[3], res[4])
+
+def remove_clothes(id):
+	st = "DELETE FROM wb_clothes WHERE id = %s"
+	vals = [id]
+	db.execute(st, vals)
+	confirm()
+
+
+def set_screen(uuid, screen):
+	st = "UPDATE wb_users SET screen = %s WHERE tg_id = %s"
+	vals = [screen, uuid]
+	db.execute(st, vals)
+	confirm()
+
+def generate_inline_keyboard(buttons):
+	keyboard = types.InlineKeyboardMarkup()
+	for item in buttons:
+		temp_btn = types.InlineKeyboardButton(text = item['text'], callback_data = item['callback'])
+		keyboard.add(temp_btn)
+	return keyboard
+
 def set_location(uuid, location):
-	st = "UPDATE wb_users SET city = NULL, lat = %s, lon = %s, screen = 0 WHERE tg_id = %s"
-	vals = [location.latitude, location.longitude, uuid]
+	st = "UPDATE wb_users SET lat = %s, lon = %s, screen = 0 WHERE tg_id = %s"
+	vals = [location['latitude'], location['longitude'], uuid]
 	db.execute(st, vals)
 	confirm()
 
 	send_success(uuid)
 def set_city(uuid, text):
-	st = "UPDATE wb_users SET city = %s, lat = NULL, lon = NULL, screen = 0 WHERE tg_id = %s"
-	vals = [text, uuid]
-	db.execute(st, vals)
-	confirm()
+	geo = geolocation(user_agent="wbot")
+	tmp_location = geo.geocode(text)
+	try:
+		set_location(uuid, {
+			'latitude': tmp_location.latitude,
+			'longitude': tmp_location.longitude
+		})
+	except AttributeError:
+		bot.send_message(uuid, f"Неверно указан город, попробуйте ещё раз ❌", reply_markup = no_kb)
+		set_screen(uuid, 1)
 
-	send_success(uuid)
-
-def send_weather(uuid):
-	st = "SELECT city, lat, lon FROM wb_users WHERE tg_id = %s"
-	vals = [uuid]
-	db.execute(st, vals)
-
-	data = db.fetchall()[0]
-	city = data[0]
-	lat = data[1]
-	lon = data[2]
-
-	if (city == None):
-		weather = get_weather_location(lat, lon)
-	else:
-		weather = get_weather_city(city)
-
-	if (weather['cod'] == '404'):
-		message = "Город не найден ❌"
-	elif (weather['cod'] == 200):
-		message = f"""
-Температура: {weather['main']['temp']} С°
-Ощущается как: {weather['main']['feels_like']} С°
-Влажность: {weather['main']['humidity']}%
-Сейчас на улице {weather['weather'][0]['description']}.
-		"""
-	else:
-		message = f"❌ API Error: {weather['cod']} - {weather['message']}"
-	
-	bot.send_message(uuid, message)
-
-
-def deemojify(text):
-	import re
-	regrex_pattern = re.compile(pattern = "["
-		u"\U0001F600-\U0001F64F"  # emoticons
-		u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-		u"\U0001F680-\U0001F6FF"  # transport & map symbols
-		u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-								"]+", flags = re.UNICODE)
-	return regrex_pattern.sub(r'',text)
 
 @bot.message_handler(commands=['start'])
-def welcome(message):
+def start(message):
 	uuid = message.chat.id
 
 	st = "SELECT * FROM wb_users WHERE tg_id = %s"
@@ -107,29 +168,25 @@ def welcome(message):
 		
 
 	if (res == []):
-		bot.send_message(uuid, "Здравствуйте! Для продолжения отправьте название своего города или своё местоположение")
+		bot.send_message(uuid, "Здравствуйте! Для продолжения отправьте название своего города или своё местоположение", reply_markup = no_kb)
 
 		st = "INSERT INTO wb_users (tg_id, screen) VALUES (%s, %s)"
 		db.execute(st, [uuid, 1])
 		confirm()
 	
 	else:
-		if check_active(uuid):
-			if get_screen(uuid) == 0:
-				send_weather(uuid)
-			else:
-				send_idk(uuid)
-
+		send_weather(uuid)
 @bot.message_handler(content_types=['location'])
-def lalala(message):
+def location(message):
 	uuid = message.chat.id
 	if check_active(uuid):
 		if (get_screen(uuid) == 1):
-			set_location(uuid, message.location)
-		
-
+			set_location(uuid, {
+				'latitude': message.location.latitude,
+				'longitude': message.location.longitude
+			})
 @bot.message_handler(commands=['deactivate'])
-def welcome(message):
+def deactivate(message):
 	uuid = message.chat.id
 
 
@@ -141,9 +198,8 @@ def welcome(message):
 	res = db.fetchall()
 
 	bot.send_message(uuid, f"✅ Успешно")
-
 @bot.message_handler(commands=['activate'])
-def welcome(message):
+def activate(message):
 	uuid = message.chat.id
 
 	st = "UPDATE `wb_users` SET `active` = true, `screen` = 0 WHERE `tg_id` = %s"
@@ -153,10 +209,9 @@ def welcome(message):
 
 	res = db.fetchall()
 
-	bot.send_message(uuid, f"✅ Успешно")
-
+	send_success(uuid)
 @bot.message_handler(commands=['setlocation'])
-def welcome(message):
+def setlocation(message):
 	uuid = message.chat.id
 	if check_active(uuid):
 		st = "UPDATE wb_users SET screen = 1 WHERE tg_id = %s"
@@ -164,18 +219,263 @@ def welcome(message):
 		db.execute(st, vals)
 		confirm()
 
-		bot.send_message(uuid, "Отправьте новое местоположение")
+		bot.send_message(uuid, "Отправьте новое местоположение", reply_markup = no_kb)
 
+@bot.message_handler(commands=['my_clothes'])
+def my_clothes(message):
+	uuid = message.chat.id
+	st = "SELECT id, name, type, thing FROM wb_clothes WHERE user = %s"
+
+	vals = [uuid]
+	db.execute(st, vals)
+	msg = "<u><b>Ваша одежда:</b></u>\n\n"
+	for item in db.fetchall():
+		if (item[2] == None or item[3] == None):
+			# remove_clothes(item[0])
+			pass
+		else:
+			c_name = item[1]
+			c_description = get_clothes_description(item[2], item[3])
+			c_type = c_description[0]
+			c_thing = c_description[1]
+			
+			msg += f"– {c_name}, тип: {c_type}, вид: {c_thing}\n"
+
+	bot.send_message(uuid, msg, parse_mode = 'html', reply_markup = main_kb)
+
+
+@bot.message_handler(commands=['edit_clothes'])
+def edit_clothes(message, offset = 0, edit = False, msg_id = -1):
+	uuid = message.chat.id
+	st = "SELECT COUNT(id) FROM wb_clothes WHERE user = %s"
+	vals = [uuid]
+	
+
+	db.execute(st, vals)
+	quantity = int(db.fetchall()[0][0])
+
+	page = math.ceil(offset / 5)
+	if ((offset % 5) == 0):
+		page += 1
+	c_page = math.ceil(quantity / 5)
+
+	st = "SELECT id, name FROM wb_clothes WHERE user = %s LIMIT %s, 5"
+
+	vals = [uuid, offset]
+	db.execute(st, vals)
+
+	buttons = []
+
+	for c_id, c_name in db.fetchall():
+		buttons.append({'text': c_name, 'callback': '{"func":"edit","id":' + str(c_id) + '}'})
+	keyboard = generate_inline_keyboard(buttons)
+
+	if (page == 1):
+		keyboard.add(types.InlineKeyboardButton(text = '➡️', callback_data = '{"func":"np","o":' + str(offset) + '}'))
+	elif (page == c_page):
+		keyboard.add(types.InlineKeyboardButton(text = '⬅️', callback_data = '{"func":"pp","o":' + str(offset) + '}'))
+	else:
+		keyboard.row(
+			types.InlineKeyboardButton(text = '⬅️', callback_data = '{"func":"pp","o":' + str(offset) + '}'),
+			types.InlineKeyboardButton(text = '➡️', callback_data = '{"func":"np","o":' + str(offset) + '}')
+		)
+	
+
+	msg = f'Редактирование одежды\n\nСтраница {page} из {c_page}'
+
+	if (edit):
+		bot.edit_message_text(chat_id = uuid, message_id = msg_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+	else:
+		bot.send_message(uuid, msg, parse_mode = 'html', reply_markup = keyboard)
+
+
+@bot.message_handler(commands=['schedule'])
+def schedule(message):
+	uuid = message.chat.id
+	msg = 'Расписание'
+
+	bot.send_message(uuid, msg, parse_mode = 'html', reply_markup = main_kb)
+
+
+@bot.message_handler(commands=['add_clothes'])
+def add_clothes(message):
+	uuid = message.chat.id
+	text = "Введите название вещи"
+	set_screen(uuid, 2)
+	bot.send_message(uuid, text, reply_markup = no_kb)
+
+def choose_type(uuid, name, edit=False, msg_id=-1, thing_id=-1):
+	set_screen(uuid, 0)
+	if (edit):
+		pass
+	else:
+		st = "INSERT INTO wb_clothes (user, name) VALUES (%s, %s)"
+		vals = [uuid, name]
+		db.execute(st, vals)
+		confirm()
+		st = "SELECT LAST_INSERT_ID()"
+		db.execute(st)
+		thing_id = db.fetchall()[0][0]
+
+	msg = f"Отлично 👍\nТеперь выберете тип и вид одежды.\n\n<u><b>Название:</b></u> {name}\nТип:"
+	buttons = (
+		{'text': 'Головной убор', 'callback': '{"func":"headdress","id":' + str(thing_id) + '}'},
+		{'text': 'Верхняя одежда', 'callback': '{"func":"outerwear","id":' + str(thing_id) + '}'},
+		{'text': 'Штаны', 'callback': '{"func":"pants","id":' + str(thing_id) + '}'},
+		{'text': 'Обувь', 'callback': '{"func":"shoes","id":' + str(thing_id) + '}'},
+		{'text': 'Назад 🔙', 'callback': '{"func":"back_to_name","id":' + str(thing_id) + '}'},
+	)
+
+	keyboard = generate_inline_keyboard(buttons)
+
+	if (edit):
+		bot.edit_message_text(chat_id = uuid, message_id = msg_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+	else:
+		bot.send_message(uuid, msg, reply_markup = keyboard, parse_mode = 'html')
+
+@bot.callback_query_handler(func = lambda call: True)
+def answer(call):
+	uuid = call.message.chat.id
+	data = json.loads(call.data)
+	if (data['func'] != 'np' and data['func'] != 'pp'):
+		clothes_data = get_clothes_info(data['id'])
+		c_name = clothes_data[1]
+		c_type = clothes_data[2]
+		c_thing = clothes_data[3]
+
+	if (data['func'] == 'headdress'):
+		set_type(data['id'], 0)
+		c_type = 0
+		msg = f"Отлично 👍\nТеперь выберете тип и вид одежды.\n\n<u><b>Название:</b></u> {c_name}\n<u><b>Тип:</b></u> Головной убор\n\nВид:"
+
+		buttons = (
+			{'text': 'Тёплая шапка', 'callback': '{"func":"addc01","id":' + str(data['id']) + '}'},
+			{'text': 'Зимняя шапка', 'callback': '{"func":"addc02","id":' + str(data['id']) + '}'},
+			{'text': 'Шапка', 'callback': '{"func":"addc03","id":' + str(data['id']) + '}'},
+			{'text': 'Шапка (можно и без неё)', 'callback': '{"func":"addc04","id":' + str(data['id']) + '}'},
+			{'text': 'Кепка', 'callback': '{"func":"addc05","id":' + str(data['id']) + '}'},
+			{'text': 'Назад 🔙', 'callback': '{"func":"thing_back","id":' + str(data['id']) + '}'},
+		)
+		
+		keyboard = generate_inline_keyboard(buttons)
+
+		bot.edit_message_text(chat_id = uuid, message_id = call.message.message_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+	
+	elif (data['func'] == 'outerwear'):
+		set_type(data['id'], 2)
+		c_type = 2
+		msg = f"Отлично 👍\nТеперь выберете тип и вид одежды.\n\n<u><b>Название:</b></u> {c_name}\n<u><b>Тип:</b></u> Верхняя одежда\n\nВид:"
+
+		buttons = (
+			{'text': 'Очень тёплая куртка', 'callback': '{"func":"addc20","id":' + str(data['id']) + '}'},
+			{'text': 'Зимняя курка', 'callback': '{"func":"addc21","id":' + str(data['id']) + '}'},
+			{'text': 'Ветровка', 'callback': '{"func":"addc22","id":' + str(data['id']) + '}'},
+			{'text': 'Ветровка или худи', 'callback': '{"func":"addc23","id":' + str(data['id']) + '}'},
+			{'text': 'Свитер', 'callback': '{"func":"addc11","id":' + str(data['id']) + '}'},
+			{'text': 'Худи', 'callback': '{"func":"addc24","id":' + str(data['id']) + '}'},
+			{'text': 'Футболка', 'callback': '{"func":"addc25","id":' + str(data['id']) + '}'},
+			{'text': 'Назад 🔙', 'callback': '{"func":"thing_back","id":' + str(data['id']) + '}'},
+		)
+		
+		keyboard = generate_inline_keyboard(buttons)
+
+		bot.edit_message_text(chat_id = uuid, message_id = call.message.message_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+
+	elif (data['func'] == 'pants'):
+		set_type(data['id'], 3)
+		c_type = 3
+		msg = f"Отлично 👍\nТеперь выберете тип и вид одежды.\n\n<u><b>Название:</b></u> {c_name}\n<u><b>Тип:</b></u> Штаны\n\nВид:"
+
+		buttons = (
+			{'text': 'Тёплые штаны', 'callback': '{"func":"addc30","id":' + str(data['id']) + '}'},
+			{'text': 'Джинсы', 'callback': '{"func":"addc31","id":' + str(data['id']) + '}'},
+			{'text': 'Шорты', 'callback': '{"func":"addc32","id":' + str(data['id']) + '}'},
+			{'text': 'Назад 🔙', 'callback': '{"func":"thing_back","id":' + str(data['id']) + '}'},
+		)
+		
+		keyboard = generate_inline_keyboard(buttons)
+
+		bot.edit_message_text(chat_id = uuid, message_id = call.message.message_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+
+	elif (data['func'] == 'shoes'):
+		set_type(data['id'], 4)
+		c_type = 4
+		msg = f"Отлично 👍\nТеперь выберете тип и вид одежды.\n\n<u><b>Название:</b></u> {c_name}\n<u><b>Тип:</b></u> Обувь\n\nВид:"
+
+		buttons = (
+			{'text': 'Сапоги', 'callback': '{"func":"addc40","id":' + str(data['id']) + '}'},
+			{'text': 'Тёплые ботинки', 'callback': '{"func":"addc41","id":' + str(data['id']) + '}'},
+			{'text': 'Ботинки или кроссовки', 'callback': '{"func":"addc42","id":' + str(data['id']) + '}'},
+			{'text': 'Кроссовки', 'callback': '{"func":"addc43","id":' + str(data['id']) + '}'},
+			{'text': 'Назад 🔙', 'callback': '{"func":"thing_back","id":' + str(data['id']) + '}'},
+		)
+		
+		keyboard = generate_inline_keyboard(buttons)
+
+		bot.edit_message_text(chat_id = uuid, message_id = call.message.message_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+
+
+
+	elif (data['func'][0:4] == 'addc'):
+		c_type = data['func'][4]
+		c_thing = data['func'][5]
+
+		st = "UPDATE wb_clothes SET name = %s, type = %s, thing = %s WHERE id = %s"
+		vals = [c_name, c_type, c_thing, data['id']]
+		db.execute(st, vals)
+		confirm()
+
+		bot.send_message(chat_id = uuid, text = 'Успешно ✅', reply_markup = main_kb)
+		bot.delete_message(chat_id = uuid, message_id = call.message.message_id)
+
+
+	elif (data['func'] == 'np'):
+		edit_clothes(call.message, int(data['o']) + 5, True, call.message.message_id)
+
+	elif (data['func'] == 'pp'):
+		edit_clothes(call.message, int(data['o']) - 5, True, call.message.message_id)
+
+	elif (data['func'] == 'edit'):
+		buttons = (
+			{'text': 'Сохранить ✅', 'callback': '{"func":"addc11","id":"' + str(data['id']) + '"}'},
+			{'text': 'Сохранить ✅', 'callback': '{"func":"addc11","id":"' + str(data['id']) + '"}'},
+			{'text': 'Назад 🔙', 'callback': '{"func":"edit_back","id":"' + str(data['id']) + '"}'},
+		)
+
+
+		bot.edit_message_text(chat_id = uuid, message_id = call.message.message_id, text = msg, reply_markup = keyboard, parse_mode = 'html')
+
+	elif (data['func'] == 'thing_back'):
+		choose_type(uuid, c_name, True, call.message.message_id, data['id'])
+	elif (data['func'] == 'back_to_name'):
+		set_screen(data['id'], 2)
+		add_clothes(call.message)
+		bot.delete_message(chat_id = uuid, message_id = call.message.message_id)
 
 @bot.message_handler(content_types=['text'])
-def lalala(message):
+def text(message):
 	uuid = message.chat.id
+	screen = get_screen(uuid)
+	text = message.text
 	if check_active(uuid):
-		if (get_screen(uuid) == 1):
-			set_city(uuid, deemojify(message.text))
+		if (text == '➕ Добавить одежду'):
+			add_clothes(message)
+		elif (text == '👔 Что надеть?'):
+			send_weather(uuid)
+		elif (text == '🗄️ Моя одежда'):
+			my_clothes(message)
+		elif (text == '🏠 Сменить город'):
+			setlocation(message)
+		elif (text == '⏰ Расписание'):
+			schedule(message)
+		elif (text == '✏️ Редактировать одежду'):
+			edit_clothes(message)
 		else:
-			send_idk(uuid)
-
+			if (screen == 1):
+				set_city(uuid, text)
+			elif (screen == 2):
+				choose_type(uuid, text)
+			else:
+				send_idk(uuid)
 
 bot.polling(none_stop=True)
-
